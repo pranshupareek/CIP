aea6875b482761beecb69ee0c52f5b25776b8f10
Pranshu Pareek <pranshupareek47@gmail.com>
2020-10-31 17:25:15 +0530

    Intial Commit

---
+++ src/gph.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class gph {
+    public static void main(String arg[]) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+
+        
+
+
+
+//        int ans = 0;
+////        Sum of first N natural number = O(N)
+//        for ( int i = 1; i <= n; i++) ans += i;
+
+//        Sum of first N natural number O(1)
+//        ans = n*(n+1)/2;
+//
+//
+//        System.out.println(ans);
+    }
+}
