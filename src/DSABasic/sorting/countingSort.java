0634cc13eec2c7beabfbc82615f1093d82b8bb08
Pranshu Pareek <pranshupareek47@gmail.com>
2020-11-21 17:42:51 +0530

    counting sort with range

--- src/DSABasic/sorting/countingSort.java
+++ src/DSABasic/sorting/countingSort.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 
 public class countingSort {
 
-    public static void cSort(int arr[], int k){
+    public static void cSort(int arr[], int l, int u){
+        int k = u-l+1;
         int counter[]= new int[k];
         for (int i=0; i<k; i++){
             counter[i] = 0;
         }
         for (int i=0; i< arr.length; i++){
-            counter[arr[i]]++;
+            counter[arr[i]-l]++;
         }
         for (int i = 1; i<k; i++){
             counter[i]= counter[i-1] + counter[i];
         }
         int[] output = new int[arr.length];
         for (int i = arr.length-1; i>=0; i--){
-            output[counter[arr[i]] - 1] = arr[i];
-            counter[arr[i]]--;
+            output[counter[arr[i]-l] - 1] = arr[i];
+            counter[arr[i]-l]--;
         }
         for (int i=0; i< arr.length; i++){
             arr[i] = output[i];
@@ -33,9 +34,10 @@ public class countingSort {
     }
 
     public static void main(String arg[]){
-        int[] arr = {1, 4, 4, 1, 0, 1};
-        int k = 5;
-        cSort(arr,k);
+        int[] arr = {1, 4, 4, 1, 3, 1};
+        int l = 1;
+        int u = 4;
+        cSort(arr,l,u);
         printArray(arr, arr.length);
     }
 
