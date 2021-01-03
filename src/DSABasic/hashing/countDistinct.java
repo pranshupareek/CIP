3d2ce82f84652a5c7bdc86b5e2c8261192c526c5
Pranshu Pareek <pranshupareek47@gmail.com>
2021-01-03 17:18:22 +0530

    count distinct improved

--- src/DSABasic/hashing/countDistinct.java
+++ src/DSABasic/hashing/countDistinct.java
@@ -1,20 +1,18 @@
 package DSABasic.hashing;
 
+import java.util.Arrays;
 import java.util.HashSet;
 
 public class countDistinct {
-    static int count(int[] arr){
-        HashSet<Integer> h = new HashSet<>();
-        for (int i = 0; i<arr.length; i++){
-            h.add(arr[i]);
-        }
+    static int count(Integer arr[]){
+        HashSet<Integer> h = new HashSet<>(Arrays.asList(arr));
         return h.size();
     }
 
     public static void main(String args[]){
 //        int[] arr = {15, 12, 13, 12, 13, 13, 18};
 //        int[] arr = {10, 10, 10};
-        int[] arr = {10, 11, 12};
+        Integer[] arr = {10, 11, 12};
         System.out.println(count(arr));
     }
 }
