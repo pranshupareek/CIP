ee5bccab8248e94cfbc05ee96467de81b270928a
Pranshu Pareek <pranshupareek47@gmail.com>
2021-01-06 17:05:37 +0530

    recursive traversal in ll

--- src/DSABasic/linkedlist/simpleLL.java
+++ src/DSABasic/linkedlist/simpleLL.java
@@ -17,12 +17,20 @@ public class simpleLL {
             temp = temp.next;
         }
     }
+    static void printLLRec(Node head){
+        if (head!=null){
+            System.out.print(head.data+ " ");
+            printLLRec(head.next);
+        }
+    }
     public static void main(String args[]){
         Node head = new Node(10);
         Node t1 = new Node(20);
         Node t2 = new Node(30);
         head.next = t1;
+//        operation
         t1.next = t2;
         printLL(head);
+        printLLRec(head);
     }
 }
