3bd401152cca13e8ff81e64266ab4af61fecec3d
Pranshu Pareek <pranshupareek47@gmail.com>
2021-01-11 19:51:31 +0530

    Insert at Begin of Doubly Linked List

---
+++ src/DSABasic/doublylinkedlist/doublyLL.java
@@ -0,0 +1,44 @@
+package DSABasic.doublylinkedlist;
+
+class Node {
+    int data;
+    Node prev;
+    Node next;
+    Node (int d){
+        data = d;
+    }
+}
+
+public class doublyLL {
+    static Node insertBegin(Node head, int data){
+        Node temp = new Node(data);
+        if (head==null){
+            return temp;
+        }
+        temp.next = head;
+        head.prev = temp;
+        return temp;
+    }
+    static void printLL(Node head){
+        if (head == null){
+            return;
+        }
+        Node temp = head;
+        while (temp != null){
+            System.out.print(temp.data+ " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void main(String arg[]){
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.prev = head;
+        head.next.next = new Node(30);
+        head.next.next.prev = head.next;
+        printLL(head);
+        System.out.println();
+        head = insertBegin(head,5);
+        printLL(head);
+    }
+}
