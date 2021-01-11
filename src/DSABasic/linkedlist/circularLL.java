9043359914a74f420fc7727339a0660d5957ef58
Pranshu Pareek <pranshupareek47@gmail.com>
2021-01-11 13:38:46 +0530

    Insert at Begin of Circular Linked List

--- src/DSABasic/linkedlist/circularLL.java
+++ src/DSABasic/linkedlist/circularLL.java
@@ -13,12 +13,29 @@ public class circularLL {
             temp = temp.next;
         }
     }
+    static Node insertAtBeg(Node head, int ele){
+        Node temp = new Node(ele);
+        if (head==null){
+            temp.next = temp;
+            return temp;
+        }
+        int tempData = head.data;
+        head.data = temp.data;
+        temp.data = tempData;
+        temp.next = head.next;
+        head.next = temp;
+        return head;
+    }
 
     public static void main(String args[]){
         Node head = new Node(10);
         head.next = new Node(20);
         head.next.next = new Node(30);
         head.next.next.next = head;
+        printLL(head);
+
+        head = insertAtBeg(head, 5);
+        System.out.println();
         printLL(head);
     }
 }
