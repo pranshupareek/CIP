e5d9fdbb18dfc308cffa902e5c9071a8f06b3dbd
Pranshu Pareek <pranshupareek47@gmail.com>
2021-01-20 23:05:54 +0530

    Floor in BST

--- src/DSABasic/bst/bst.java
+++ src/DSABasic/bst/bst.java
@@ -88,6 +88,25 @@ public class bst {
         }
         return curr;
     }
+    
+    static int floor(binaryTree bt, int ele){
+        binaryTree prev = null;
+        while (bt!=null){
+            if (bt.key==ele){
+                return ele;
+            }
+            else if (bt.key>ele){
+                bt = bt.left;
+            }
+            else {
+                prev = bt;
+                bt = bt.right;
+            }
+        }
+        if (prev!=null)
+        return prev.key;
+        else return -1;
+    }
 
     public static void main(String arg[]){
         binaryTree bt = new binaryTree(15);
@@ -104,5 +123,7 @@ public class bst {
         System.out.println();
         bt = delNode(bt, 15);
         inorder(bt);
+        System.out.println();
+        System.out.println(floor(bt, 1));
     }
 }
