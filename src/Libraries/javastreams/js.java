9089c60163d5eacc203339f4316191908a9b4981
Pranshu Pareek <pranshupareek47@gmail.com>
2021-01-26 15:35:05 +0530

    Print first n natural numbers with given digit d

---
+++ src/Libraries/javastreams/js.java
@@ -0,0 +1,12 @@
+package Libraries.javastreams;
+import java.util.stream.Stream;
+
+
+public class js {
+    public static void main (String[] args) {
+        Stream.iterate(1, x -> x + 1)
+                .filter(x -> x.toString().contains("5"))
+                .limit(10)
+                .forEach(System.out::println);
+    }
+}
