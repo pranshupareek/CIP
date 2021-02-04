4b0b54e2a8d0b366b0a712552fa4203d391f8c27
Pranshu Pareek <pranshupareek47@gmail.com>
2021-02-04 18:12:39 +0530

    Stock Buy and Sell problem

---
+++ src/DSAAdvance/arrays/stockBuySell.java
@@ -0,0 +1,19 @@
+package DSAAdvance.arrays;
+
+public class stockBuySell {
+    static void profit(int arr[]){
+        int profit = 0;
+        int n = arr.length;
+        for (int i = 1; i<n; i++){
+            if (arr[i]>arr[i-1]){
+                profit+=(arr[i]-arr[i-1]);
+            }git
+        }
+        System.out.println(profit);
+    }
+
+    public static void main(String arg[]){
+        int arr[] = {5,50,2,3,8,1,4};
+        profit(arr);
+    }
+}
