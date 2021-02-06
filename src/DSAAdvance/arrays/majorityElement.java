b112fe39caa4579bbdbd3b11797b324fd69d0c5c
Pranshu Pareek <pranshupareek47@gmail.com>
2021-02-06 13:47:19 +0530

    Majority Element Efficient Solution

--- src/DSAAdvance/arrays/majorityElement.java
+++ src/DSAAdvance/arrays/majorityElement.java
@@ -4,6 +4,31 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class majorityElement {
+    static int majorityNoExtraSpace(int arr[]){
+        int res = 0, count = 1, n = arr.length;
+        for (int i = 1; i<n; i++){
+            if (arr[res] == arr[i]){
+                count++;
+            }
+            else {
+                count--;
+            }
+            if (count==0){
+                res = i;
+                count = 1;
+            }
+        }
+        count = 0;
+        for (int i = 0; i<n; i++){
+            if (arr[res] == arr[i]){
+                count++;
+            }
+        }
+        if (count>n/2){
+            return res;
+        }
+        return -1;
+    }
     static int majority(int arr[]){
         int n = arr.length;
         Map<Integer, Integer> hm = new HashMap<>();
@@ -19,5 +44,6 @@ public class majorityElement {
     public static void main(String arg[]){
         int arr[] = {20,30,40,50,50,50,50};
         System.out.println(majority(arr));
+        System.out.println(majorityNoExtraSpace(arr));
     }
 }
