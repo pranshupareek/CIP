e212f3e069070e031c88ec1e9367c7866db9ae44
Pranshu Pareek <pranshupareek47@gmail.com>
2021-02-07 13:56:16 +0530

    N-bonacci numbers

--- src/DSAAdvance/arrays/windowSlidingTechnique.java
+++ src/DSAAdvance/arrays/windowSlidingTechnique.java
@@ -1,5 +1,8 @@
 package DSAAdvance.arrays;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class windowSlidingTechnique {
     static int maxSumOfKElements(int arr[], int k){
         int n = arr.length;
@@ -44,6 +47,24 @@ public class windowSlidingTechnique {
         return false;
     }
 
+    static void printNBonacci(int n, int m){
+        Queue<Integer> q = new ArrayDeque<>(n);
+        for (int i = 1; i<n ; i++){
+            q.add(0);
+        }
+        q.add(1);
+        int curSum = 1;
+        for (int i = 0; i<m; i++){
+            int remove = q.poll();
+            System.out.print(remove+" ");
+            int temp = curSum;
+            q.add(temp);
+            curSum-=remove;
+            curSum+=temp;
+        }
+        System.out.println();
+    }
+
     public static void main(String arg[]){
         int arr[] = {1,8,30,-5,20,7};
         int k = 3;
@@ -52,6 +73,8 @@ public class windowSlidingTechnique {
         int arr2[] = {1,4,20,3,10,5};
         int sum = 18;
         System.out.println(haveGivenSum(arr2,sum));
+
+        printNBonacci(4,10);
 
     }
 }
