2f827c1ac7f443882d4760e4f5df0defb5ce14a8
Pranshu Pareek <pranshupareek47@gmail.com>
2021-02-09 22:15:18 +0530

    Square root

---
+++ src/DSAAdvance/searching/squareRoot.java
@@ -0,0 +1,25 @@
+package DSAAdvance.searching;
+
+public class squareRoot {
+    static int squareRoot(int x){
+        int low = 1, high = x, ans = -1;
+        while (low<=high){
+            int mid = (low+high)/2;
+            int mSq = mid*mid;
+            if (mSq==x){
+                return mid;
+            }
+            else if (mSq>x)
+                high = mid - 1;
+            else {
+                low = mid +1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String arg[]){
+        System.out.println(squareRoot(69));
+    }
+}
