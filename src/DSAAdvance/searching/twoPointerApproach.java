2bd9b82d1c6added85ea804cb72dd76f0c3efee7
Pranshu Pareek <pranshupareek47@gmail.com>
2021-02-11 23:50:30 +0530

    Find triplet in an array which gives sum X

--- src/DSAAdvance/searching/twoPointerApproach.java
+++ src/DSAAdvance/searching/twoPointerApproach.java
@@ -17,8 +17,30 @@ public class twoPointerApproach {
         }
         return false;
     }
+    static boolean isTripletSum(int arr[], int sum){
+        int l, r, mid;
+        for (int i =0; i< arr.length; i++){
+            l = i;
+            r = arr.length-1;
+            mid = l+1;
+            while (mid<r){
+                if (arr[l]+arr[mid]+arr[r] == sum){
+                    return true;
+                }
+                else if (arr[l]+arr[mid]+arr[r] < sum){
+                    mid++;
+                }
+                else {
+                    r--;
+                }
+            }
+        }
+        return false;
+    }
     public static void main(String arg[]){
         int arr[] = {2,5,8,12,30};
         System.out.println(checkPairSum(arr,8));
+
+        System.out.println(isTripletSum(arr,47));
     }
 }
