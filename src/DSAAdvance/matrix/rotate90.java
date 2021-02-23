656df799984eb77a1b63580a35cb3d842e0e86a3
Pranshu Pareek <46353918+pranshupareek@users.noreply.github.com>
2021-02-23 15:03:24 +0000

    Rotate Matrix Anti-clockwise by 90

--- src/DSAAdvance/matrix/rotate90.java
+++ src/DSAAdvance/matrix/rotate90.java
@@ -1,12 +1,12 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class transpose {
+public class rotate90 {
     public static void swap(int arr[][],int r1, int c1, int r2, int c2){
         int temp = arr[r1][c1];
         arr[r1][c1] = arr[r2][c2];
         arr[r2][c2] = temp;
     }
-    static void makeTranspose(int arr[][]) {
+    public static void makeTranspose(int arr[][]) {
         int r = arr.length;
         int c = arr[0].length;
         for (int i = 0; i<r; i++) {
@@ -14,11 +14,22 @@ public class transpose {
                 swap(arr,i,j,j,i);
             }
         }
-    }    
+    }   
+    static void rotateArray(int arr[][]){
+        makeTranspose(arr);
+        int r = arr.length;
+        int c = arr[0].length;
+        for (int i = 0; i<r/2; i++) {
+            for (int j = 0; j<c; j++) {
+                swap(arr, i, j, r-i-1, j);
+            }
+        }
+    }
     public static void main(String[] args) {
         int arr[][] = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
-        makeTranspose(arr);
+        rotateArray(arr);
         for (int i = 0; i<arr.length; i++)
         System.out.println(Arrays.toString(arr[i]));
     }
+    
 }
