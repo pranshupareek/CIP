82844c1b9dc5a4a540e7e7c0500173578663c456
Pranshu Pareek <46353918+pranshupareek@users.noreply.github.com>
2021-03-13 16:57:23 +0000

    Detect Loop using hashing in LL

---
+++ src/DSAAdvance/linkedlist/detectLoopInLL.java
@@ -0,0 +1,42 @@
+package DSAAdvance.linkedlist;
+
+import java.util.*;
+
+class Node{
+    public int val;
+    public Node next;
+    Node(int ele){
+        val = ele;
+        next = null;
+    }
+}
+
+public class detectLoopInLL {
+    static boolean detectLoopHashing(Node head){
+        Set<Node> hm = new HashSet<>();
+        Node cur = head;
+        while (cur.next!=null){
+            if (hm.contains(cur.next)){
+                return true;
+            }
+            hm.add(cur);
+            cur = cur.next;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Node head = new Node(10);
+        head.next = new Node(20);
+        head.next.next = new Node(30);
+        head.next.next.next = new Node(40);
+        head.next.next.next.next = new Node(50);
+        head.next.next.next.next.next = new Node(60);
+        head.next.next.next.next.next.next = new Node(70);
+        head.next.next.next.next.next.next.next = new Node(80);
+        head.next.next.next.next.next.next.next.next = new Node(90);
+        head.next.next.next.next.next.next.next.next.next = head.next.next.next.next.next.next.next.next;
+
+        System.out.println(detectLoopHashing(head));
+
+    }
+}
