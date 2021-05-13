37a9fa92cce65b99d415b00e062437d2b2b9882f
pranshupareek <pranshupareek47@gmail.com>
2021-05-13 16:59:07 +0530

    dp solution for coin count

--- src/DSAAdvance/dp/coinChangeCountCombinations.java
+++ src/DSAAdvance/dp/coinChangeCountCombinations.java
@@ -18,6 +18,25 @@ public class coinChangeCountCombinations {
         }
     }
 
+    static int countDP(int coins[], int n, int sum){
+        int dp[][] = new int[sum+1][n+1];
+
+        for (int i = 0; i<=n; i++){
+            dp[0][i]=1;
+        }
+
+        for (int i = 1; i<=sum; i++){
+            for (int j = 1; j<=n; j++){
+                dp[i][j] = dp[i][j-1];
+
+                if (coins[j-1]<=i){
+                    dp[i][j]+=dp[i-coins[j-1]][j];
+                }
+            }
+        }
+        return dp[sum][n];
+    }
+
     public static void main(String[] args) {
         int coins[] = {1,2,3};
         int res[] = new int[1];
@@ -27,5 +46,7 @@ public class coinChangeCountCombinations {
         res[0] = 0;
         count(coins2, 0, 0, 10, res);
         System.out.println(res[0]);
+
+        System.out.println(countDP(coins, coins.length, 4));
     }
 }
